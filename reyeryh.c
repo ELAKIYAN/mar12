399970c0c479d442d9798dd81a8d7c5152eac1fd
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-03-14 16:11:51 +0530

    Update reyeryh.c

--- reyeryh.c
+++ reyeryh.c
@@ -2,11 +2,11 @@
 
 int main(void) 
 {
-	int n,m,i,j;
-	scanf("%d %d",&n,&m);
-	int a[10];
-	for(i=0;i<n;i++)
-	{
+	 int n,m,i,j;
+	 scanf("%d %d",&n,&m);
+	 int a[10];
+	 for(i=0;i<n;i++)
+	 {
 		scanf("%d",&a[i]);
 	}
 	for(i=0;i<n;i++)
